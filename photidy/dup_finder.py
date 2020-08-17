424a56d1bc3c4df59a5a1f4531f5c8393aba6a77
robbie29 <37903975+robbie29@users.noreply.github.com>
2020-08-17 20:05:10 +1000

    added methods to select base and comparison photo directories

--- photidy/dup_finder.py
+++ photidy/dup_finder.py
@@ -9,6 +9,22 @@ class photo_dup_finder(my_photo_gallery):
         """
         my_photo_gallery.__init__(self, photo_dir)
         self.base_dir = None
+        self.base_df = None
         self.comp_dirs = None
+        self.comp_df = None
         self.dup_methods = []
         self.delete_method = "careful"
+
+    def set_base_dir(self, dir_path):
+        """
+        """
+        self.base_dir = dir_path
+        q = "dir_path == @dir_path"
+        self.base_df = self.photo_df.copy().query(q)
+
+    def set_comp_dirs(self, dir_list):
+        """
+        """
+        self.comp_dirs = dir_list
+        q = "dir_path in @dir_list"
+        self.comp_df = self.photo_df.copy().query(q)
