7cc6373d256e6ba3fb3ef7d657638d0bc414e034
robbie29 <37903975+robbie29@users.noreply.github.com>
2020-11-24 13:08:15 +1100

    rename file to reflect it contains only PCA compress func

--- photidy/compress_pca.py
+++ photidy/compress_pca.py
@@ -1,28 +1,10 @@
 import numpy as np
 import matplotlib.image as mpimg
 from sklearn.decomposition import PCA
+from photidy.compress import _make_photo_bw_, _new_photo_dims_
 
-def _make_photo_bw_(img):
-    """
-    """
-    img = img.sum(axis = 2)
-    img = img/img.max()
-    return img
-
-def _new_photo_dims_(shp, max_pix):
-    """
-    Sets dimensions of compressed photo version.
-    """
-    if shp[1] > shp[0]:
-        dim0 = np.ceil((shp[0]/shp[1])*max_pix)
-        dim1 = max_pix
-    else:
-        dim0 = max_pix
-        dim1 = np.ceil((shp[1]/shp[0])*max_pix)
-    return int(dim0), int(dim1)
-
-def compress_photo(p, max_pix=50, keep_pca=False,
-                   always_portrait=True):
+def pca_compress_photo(p, max_pix=50, keep_pca=False,
+                       always_portrait=True):
     """
     Compresses photo using the PCA method.
     """
